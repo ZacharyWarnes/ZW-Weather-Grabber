c5d7dd5892869febdb804e877609330b70378809
Zachary Warnes <zacharywarnes@gmail.com>
2022-07-21 10:54:09 -0700

    starting to build js function and psueudo code after watching office hours

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 var APIkey = "8bdf30542457a49d91637d1b7a6f89e0";
+
+//app will need to use API twice. First request is to see latitute/longitude numbers (GEOCODING API), second reqeust weather information (ONE CALL API)
+
+//Handle button clicks to fetch weather information
+
+//Fetch Geolocation Data (Geocoding API)
+
+//Fetch Weather Data (ONECALL)
+
+function geoLocate () {
+   
+    var location = 1;
+
+    myFuncB(location);
+}
+
+function myWeather(location) {
+
+}
