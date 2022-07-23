cc8a738fc21247313a8900fc2656e9fff1091f99
Zachary Warnes <zacharywarnes@gmail.com>
2022-07-22 23:35:42 -0700

    added some html framework for the layout of the page. started my functions for using the API to fetch data per class instructional

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 var APIkey = "8bdf30542457a49d91637d1b7a6f89e0";
 
-//app will need to use API twice. First request is to see latitute/longitude numbers (GEOCODING API), second reqeust weather information (ONE CALL API)
+//Submit the form to fetch the weather information 
+
+    //Fetch the city name from the text <input>
+    
+    // Call the fetchGeoLocation and pass the city name 
 
 //Handle button clicks to fetch weather information
 
+    //get the city name from the clicked buttoin's data-city attribute (event.target)
+
 //Fetch Geolocation Data (Geocoding API)
+function fetchGeoLocation(cityName) {
+
+    var reqeust= `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=8bdf30542457a49d91637d1b7a6f89e0`;
+
+    fetch(reqeust)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+
+}
+
+fetchGeoLocation("Seattle");
 
 //Fetch Weather Data (ONECALL)
 
-function geoLocate () {
-   
-    var location = 1;
+function fetchOneCallWeather()
 
-    myFuncB(location);
-}
-
-function myWeather(location) {
-
-}
+//Render Function for returning data to page
