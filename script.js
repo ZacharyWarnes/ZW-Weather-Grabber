0b483c17c93d2bf55ae58a00972a7acc61810adc
Zachary Warnes <zacharywarnes@gmail.com>
2022-07-23 09:01:20 -0700

    added event listener for input button to pass input to geolocate API function. passed those fetch results to onecall weather function

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 var APIkey = "8bdf30542457a49d91637d1b7a6f89e0";
 
 //Submit the form to fetch the weather information 
+var userInput = document.getElementById("userInput");
+var userInputEl = document.getElementById("cname");
+
+function getCityName(event){
+    event.preventDefault();
+    var cityName = userInputEl.value;
+    console.log(cityName);
+
+    fetchGeoLocation(cityName);
+
+}
+
 
     //Fetch the city name from the text <input>
     
@@ -21,14 +33,29 @@ function fetchGeoLocation(cityName) {
     })
     .then(function(data) {
         console.log(data);
+        var latitude = data[0].lat; 
+        var longitude = data[0].lon; 
+fetchOneCallWeather(latitude,longitude);
     })
 
 }
 
-fetchGeoLocation("Seattle");
+// fetchGeoLocation("Seattle");
 
 //Fetch Weather Data (ONECALL)
 
-function fetchOneCallWeather()
+function fetchOneCallWeather(latitude,longitude){
+console.log(latitude,longitude);
+    var request2= `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=8bdf30542457a49d91637d1b7a6f89e0&exclude=minutely`
 
-//Render Function for returning data to page
+    // fetch(request2)
+    // .then(function(response) {
+    //     return response.json();
+    // })
+    // .then(function(data) {
+    //     console.log(data);
+    // })
+}
+//Render Function for returning data to page   
+
+userInput.addEventListener("submit", getCityName);
