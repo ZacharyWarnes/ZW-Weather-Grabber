d9edca6a08c74e59b2fabbc293581e9b2c49ef70
Zachary Warnes <zacharywarnes@gmail.com>
2022-08-01 15:59:50 -0700

    adding JS content for Current Weather Card and appended to HTML

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var userInput = document.getElementById("userInput");
 var userInputEl = document.getElementById("cname");
 var searchHistory = [];
 var recentSearchContainerEL= document.getElementById("recentSearches");
+var currentWeatherCard = document.getElementById("weatherCard");
+var forcastCard = document.getElementById("forecast");
 
 // Timezone plugins for day.js
 dayjs.extend(window.dayjs_plugin_utc);
@@ -62,7 +64,7 @@ function renderItems(locationName, data) {
 
 // Render function for current weather and variables for creating current weather card
 function renderCurrentWeather (locationName,current,timezone) {
-    var currentDate = dayjs().tz(timezone).format('MM/DD/YYY');
+    var currentDate = dayjs().tz(timezone).format('MM/DD/YYYY');
     var currentTemp = data.current.temp;
     var currentWind = data.current.wind_speed;
     var currentHumid = data.current.humidity;
@@ -71,16 +73,42 @@ function renderCurrentWeather (locationName,current,timezone) {
     var iconDescription = data.current.weather[0].description || data.current.weather[0].main;
 
      console.log(currentDate,currentTemp,currentWind,currentHumid,currentUv);
-
+//Elements that will appear in the Current Weather Card
     var cityNameEl = document.createElement('h3');
     var weatherIcon = document.createElement('img');
     var tempEl = document.createElement('p');
     var windEl = document.createElement('p');
     var humidityEl = document.createElement('p');
-    var uvIndexElk = document.createElement('p');
+    var uvIndexEl = document.createElement('p');
+    var uvButton = document.createElement('button');
 
-    
-    
+//Code for setting up weather icon and creating button for UV indicator
+    weatherIcon.setAttribute('src',iconUrl);
+    weatherIcon.setAttribute('alt', iconDescription);
+    uvButton.classList.add('btn','btn-sm');
+
+//Code for dynamic UV indicator 
+if (currentUv < 3) {
+   uvButton.classList.add('btn-success');
+} else if (currentUv < 7) {
+    uvButton.classList.add('btn-warning');
+} else {
+    uvButton.classList.add('btn-danger');
+}
+
+//Content of the Current Weather card 
+cityNameEl.textContent = `${locationName} (${currentDate})`;
+tempEl.textContent = `Temp: ${currentTemp}`;
+humidityEl.textContent = `Humid: ${currentHumid}`;
+uvIndexEl.textContent = `UV Index: `;
+uvButton.textContent = currentUv;
+uvIndexEl.append(uvButton);
+
+//Append Current Weather Card to page 
+currentWeatherCard.innerHTML = "";
+currentWeatherCard.append(cityNameEl,weatherIcon,tempEl,humidityEl,uvIndexEl);
+
+
 }
 
 function renderForcast(){
@@ -115,7 +143,6 @@ if (!returnedLocalStorage) {
     
 }
 }
-
 
 renderLocalStorage();
 userInput.addEventListener("submit", getCityName);
